ef6fd506264a0fe71fff0d27b42eea2e4e91fbd6
Xeleolon <nathan.cattell@outlook.com>
2026-01-24 21:55:22 +0800

    SpaceVogageV0.14
    Issues with wall dection not working

--- SpacePirates/Assets/Scipts/Movements/SlimeEmeny.cs
+++ SpacePirates/Assets/Scipts/Movements/SlimeEmeny.cs
@@ -8,6 +8,9 @@ public class SlimeEmeny : BaseMovement
     [Header("Damage")]
     [SerializeField] int playerDamage = 1;
     [SerializeField] int breakableDamage = 1;
+    [SerializeField] float rayCastRange = 1;
+    [SerializeField] LayerMask nullAvoidance;
+    [SerializeField] string wallObject = "wall";
 
     bool damage;
 
@@ -16,6 +19,8 @@ public class SlimeEmeny : BaseMovement
         base.Start();
         useTime = false;
     }
+    int[] availableDirection = new int[0];
+    RaycastHit2D[] hit = new RaycastHit2D[4];
     private void Update()
     {
         if (movementHoldClock <= 0)
@@ -28,40 +33,91 @@ public class SlimeEmeny : BaseMovement
         {
             movementHoldClock -= 1 * Time.deltaTime;
         }
-
         void randomMovment()
         {
-            Vector2 direction = Vector2.zero;
-            int randomDirection = Random.Range(0, 4);
-            switch (randomDirection)
-            {
-                case 1:
-                    direction = Vector2.up;
-                    break;
-                case 2:
-                    direction = Vector2.right;
-                    break;
-                case 3:
-                    direction = Vector2.down;
-                    break;
-                case 4:
-                    direction = Vector2.left;
-                    break;
 
+            //int[] availableDirection = new int[0];
+            //RaycastHit2D[] hit = new RaycastHit2D[4];
+            Vector2 direction = Vector2.zero;
+
+            for (int i = 0; i < hit.Length; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        direction = Vector2.up;
+                        break;
+                    case 2:
+                        direction = Vector2.right;
+                        break;
+                    case 3:
+                        direction = Vector2.down;
+                        break;
+                    case 4:
+                        direction = Vector2.left;
+                        break;
+
+                }
+                hit[i] = Physics2D.Raycast(transform.position, direction, rayCastRange, nullAvoidance);
+                if ((hit[i] && hit[i].transform.tag != wallObject) || !hit[i])
+                {
+                    int[] tempDirections = new int[availableDirection.Length + 1];
+                    if (availableDirection.Length > 1)
+                    {
+                        tempDirections = availableDirection;
+                    }
+                    int placement = availableDirection.Length - 1;
+                    if (placement < 0)
+                    {
+                        placement = 0;
+                    }
+                    
+                    tempDirections[placement] = i + 1;
+                    availableDirection = tempDirections;
+                }
             }
 
-            UpdateMove(direction);
+            if (availableDirection.Length > 0)
+            {
+                int randomDirection = availableDirection[Random.Range(0, availableDirection.Length)];
 
+                switch (randomDirection)
+                {
+                    case 1:
+                        direction = Vector2.up;
+                        break;
+                    case 2:
+                        direction = Vector2.right;
+                        break;
+                    case 3:
+                        direction = Vector2.down;
+                        break;
+                    case 4:
+                        direction = Vector2.left;
+                        break;
 
+                }
+                Debug.Log(hit.Length + " "+ randomDirection);
+                if (hit[randomDirection - 1 ])
+                {
+                    Actack(hit[randomDirection - 1].transform.gameObject);
+                }
+                else
+                {
+                    UpdateMove(direction);
+                }
+            }
+
+            
         }
     }
 
-    void OnCollisionEnter2D(Collision2D other)
+    void Actack(GameObject actackObject)
     {
-        if (other.gameObject.tag == "Player")
+        if (actackObject.tag == "Player")
         {
             //damage Player;
-            Player temp = other.gameObject.GetComponent<Player>();
+            Player temp = actackObject.GetComponent<Player>();
             if (temp != null)
             {
                 temp.AlterHealth(-playerDamage);
@@ -69,12 +125,12 @@ public class SlimeEmeny : BaseMovement
             }
             else
             {
-                Debug.LogError("Breakable object " + other.gameObject.name + " does not have a Breakable Script acttached");
+                Debug.LogError("Breakable object " + actackObject.name + " does not have a Breakable Script acttached");
             }
         }
-        else if (other.gameObject.tag == "Breakable")
+        else if (actackObject.tag == "Breakable")
         {
-            Breakable temp = other.gameObject.GetComponent<Breakable>();
+            Breakable temp = actackObject.GetComponent<Breakable>();
             if (temp != null)
             {
                 temp.AlterHealth(-breakableDamage);
@@ -82,8 +138,13 @@ public class SlimeEmeny : BaseMovement
             }
             else
             {
-                Debug.LogError("Breakable object " + other.gameObject.name + " does not have a Breakable Script acttached");
+                Debug.LogError("Breakable object " + actackObject.name + " does not have a Breakable Script acttached");
             }
         }
     }
+
+    /*void OnCollisionEnter2D(Collision2D other)
+    {
+        Actack(other.gameObject);
+    }*/
 }
