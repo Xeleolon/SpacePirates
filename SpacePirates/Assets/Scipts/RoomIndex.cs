0dd5dc916795f39052de88e0f7c20c8696c9793e
Xeleolon <nathan.cattell@outlook.com>
2026-02-11 10:52:41 +0800

    SpaceVoyageV0.18

--- SpacePirates/Assets/Scipts/RoomIndex.cs
+++ SpacePirates/Assets/Scipts/RoomIndex.cs
@@ -10,11 +10,22 @@ public class RoomIndex : MonoBehaviour
     void OnVaildate()
     {
         //assign spellizeds fields
+        SetBreakableLists();
+
+    }
+
+    void Awake()
+    {
+        SetBreakableLists();
+    }
+
+
+    #region generate Breakable Lists
+    void SetBreakableLists()
+    {
+
         Breakable[] allBreakables = CompleteAllBreakable();
         energy = BreakableObject(Attractant.energy);
-
-
-
 
         Breakable[] BreakableObject(Attractant type)
         {
@@ -23,12 +34,15 @@ public class RoomIndex : MonoBehaviour
 
             for (int i = 0; i < allBreakables.Length; i++)
             {
+                //Debug.Log(i + " placement, attrachant is " + allBreakables[i].attractant);
                 if (allBreakables[i].attractant == type)
                 {
                     tempStorage[numbOfAttractant] = allBreakables[i];
-                    numbOfAttractant =+ 1;
+                    numbOfAttractant = numbOfAttractant + 1;
                 }
             }
+
+            //Debug.Log("num of attractant = " + numbOfAttractant + ", tempStorage Length" + tempStorage.Length);
 
             Breakable[] finalizedBreakables = new Breakable[numbOfAttractant];
             for (int i = 0; i < numbOfAttractant; i++)
@@ -47,14 +61,47 @@ public class RoomIndex : MonoBehaviour
             {
                 tempStorage[i] = avalableBreakable[i];
             }
-            
+
             for (int i = 0; i < doors.Length; i++)
             {
                 tempStorage[avalableBreakable.Length + i] = doors[i];
             }
             return tempStorage;
         }
+    }
+    #endregion
 
-        
+    Transform CheckBreakableLists(Attractant type, Vector3 location)
+    {
+        switch (type)
+        {
+            case Attractant.energy:
+                return energy[CheckingLists(energy)].transform;
+
+            case Attractant.player:
+
+                return null ;
+        }
+
+        int CheckingLists(Breakable[] checkthis)
+        {
+            int placementTarget = 0;
+            float valueTarget = 0;
+
+            for (int i = 0; i < checkthis.Length; i++)
+            {
+                float distanceModifier= Vector3.Distance(checkthis[i].transform.position, location);
+                distanceModifier = distanceModifier / 100;
+                if ((checkthis[i].attractiveMeter + distanceModifier) > valueTarget);
+                {
+                    placementTarget = i;
+                    valueTarget = checkthis[i].attractiveMeter + distanceModifier;
+                }
+
+            }
+            return placementTarget;
+        }
+
+        return null;
     }
 }
