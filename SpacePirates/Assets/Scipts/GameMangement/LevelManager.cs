fe47b2d8da7e1473ade83d56b2d6162f946b6430
Xeleolon <nathan.cattell@outlook.com>
2026-02-13 11:25:09 +0800

    SpaceVoyage0.24
    RoomSelectorFunctional

--- SpacePirates/Assets/Scipts/GameMangement/LevelManager.cs
+++ SpacePirates/Assets/Scipts/GameMangement/LevelManager.cs
@@ -35,27 +35,39 @@ public class LevelManager : MonoBehaviour
             rooms[i].SetBreakableLists();
         }
 
-
         void AttractanctUpdateForDoors(Attractant type)
         {
-            Breakable[] typeBreakable = new Breakable[allDoors.Length];
-            int[] typeValues = new int[allDoors.Length];
-            for (int eachDoor = 0; eachDoor < allDoors.Length; eachDoor++)
+            Breakable[] typeBreakable = new Breakable[allDoors.Length]; //temp storage for alternation
+            int[] typeValues = new int[allDoors.Length]; //temp storage for alternation
+
+
+
+            for (int eachDoor = 0; eachDoor < allDoors.Length; eachDoor++) //grab highest value
             {
                 typeBreakable[eachDoor] = allDoors[eachDoor].GiveBreakaable(type); //this is the highestvalue breakable for the door
-                typeValues[eachDoor] = allDoors[eachDoor].GiveValue(type);
+                typeValues[eachDoor] = typeBreakable[eachDoor].GiveValue(type);
             }
 
-            for (int eachDoor = 0; eachDoor < allDoors.Length; eachDoor++)
+
+
+            for (int eachDoor = 0; eachDoor < allDoors.Length; eachDoor++) //create a list of all available doors with their values
             {
-                int[] loopCopyOfTypeValues = typeValues;
+                int[] loopCopyOfTypeValues = new int[typeValues.Length];
+                for (int copyArraryLoop = 0; copyArraryLoop < typeValues.Length; copyArraryLoop ++)
+                {
+                    loopCopyOfTypeValues[copyArraryLoop] = typeValues[copyArraryLoop];
+                }
+
 
                 for (int alterCopy = 0; alterCopy < loopCopyOfTypeValues.Length; alterCopy++) //alter the copid list values for the door offeset for neiber
                 {
                     //Debug.Log("at start door" + eachDoor + ", has for door" + alterCopy + " a score of " + loopCopyOfTypeValues[alterCopy]);
+
                     int tempAlter = loopCopyOfTypeValues[alterCopy]; //alter values for the door
                     tempAlter = tempAlter - (allDoors[eachDoor].neighbooringDoors[alterCopy] * sensorDeperation); //alter values for the door
 
+                    //Debug.Log("stage 3," + alterCopy + " door" + eachDoor + " has a value of " + typeValues[alterCopy]);
+                    //Debug.Log("stage 3," + alterCopy + " doorCopy" + eachDoor + " has a value of " + loopCopyOfTypeValues[alterCopy]);
 
                     if (loopCopyOfTypeValues[alterCopy] == 0)
                     {
@@ -69,20 +81,21 @@ public class LevelManager : MonoBehaviour
                     {
                         loopCopyOfTypeValues[alterCopy] = tempAlter;
                     }
+
+                    
                     //Debug.Log("at end door" + eachDoor + ", has for door" + alterCopy + " a score of " + loopCopyOfTypeValues[alterCopy]);
                 }
 
 
-                int highScorer = 0;
-                int placement = 0;
+                int highScorer = typeValues[eachDoor]; //set check as min orrginal door
+                int placement = eachDoor; //get placement form orignal door
+
+                
+
                 //find door highvalue
                 for (int highScoreLoop = 0; highScoreLoop < loopCopyOfTypeValues.Length; highScoreLoop++)
                 {
-                    if (highScoreLoop <= 0)
-                    {
-                        highScorer = loopCopyOfTypeValues[highScoreLoop];
-                    }
-                    else if (highScorer < loopCopyOfTypeValues[highScoreLoop])
+                    if (highScorer < loopCopyOfTypeValues[highScoreLoop])
                     {
                         highScorer = loopCopyOfTypeValues[highScoreLoop];
                         placement = highScoreLoop;
@@ -95,10 +108,37 @@ public class LevelManager : MonoBehaviour
                 }
 
 
-                //error need to assign neighbours only
-                if (eachDoor != placement)
+                
+                
+                if (eachDoor != placement) //don't do anything if door = placement data allready set
                 {
-                    allDoors[eachDoor].AsisgnTarget(type, allDoors[placement], typeValues[placement]);
+                    int[] neighbouringDoors = allDoors[eachDoor].neighbooringDoors;
+                    if (neighbouringDoors[placement] == 1) //asign target door if it direct neighbour already
+                    {
+                        allDoors[eachDoor].AsisgnTarget(type, allDoors[placement], typeValues[placement]);
+                    }
+                    else //isn't a neighbouring door find the door closest to final target
+                    {
+                        int neighboursplacement = findNeighbouringDoor();
+                        int neighboursValue = typeValues[placement] - (neighbouringDoors[placement] * sensorDeperation); //create values depened on the deperation as this is not a direct neigbour
+                        allDoors[eachDoor].AsisgnTarget(type, allDoors[neighboursplacement], neighboursValue);
+
+                    }
+
+                    int findNeighbouringDoor()
+                    {
+                        int targetValue = neighbouringDoors[placement];
+                        int placeOfTargetValue = placement;
+                        for (int neighbourLoop = 0; neighbourLoop < neighbouringDoors.Length; neighbourLoop++)
+                        {
+                            if (neighbouringDoors[neighbourLoop] == 1 && (targetValue > allDoors[neighbourLoop].neighbooringDoors[placement] || (targetValue == allDoors[neighbourLoop].neighbooringDoors[placement] && 0.5 > Random.Range(0, 1))))
+                            {
+                                targetValue = allDoors[neighbourLoop].neighbooringDoors[placement];
+                                placeOfTargetValue = neighbourLoop;
+                            }
+                        }
+                        return placeOfTargetValue;
+                    }
                 }
                 
 
